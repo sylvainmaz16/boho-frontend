589c46fde1035e8cc874d033ed82ef92b4f2bf20
sylvainmaz16 <sylvainmaz16@gmail.com>
2021-12-30 19:35:29 -0500

    landing images

--- components/header/LandingHeader.js
+++ components/header/LandingHeader.js
@@ -6,7 +6,7 @@ const LandingHeader = () => {
       <div className="wrapper-90 header__content">
         <div className="header__logo">
           <img
-            src="https://res.cloudinary.com/dkceblkn7/image/upload/v1637973038/boho-vendor-assets/image_2_ilwgwb.svg"
+            src="https://res.cloudinary.com/dkceblkn7/image/upload/v1639540307/boho%20vendor%20assets/image_2_1_oglnq7.svg"
             alt="logo"
           />
         </div>
