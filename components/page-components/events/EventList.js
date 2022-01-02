b239829b512f783e4e551c48664c8df1ccb55323
sylvainmaz16 <sylvainmaz16@gmail.com>
2022-01-01 16:49:17 -0800

    Migrated My-events page over to 2.0 app

---
+++ components/page-components/events/EventList.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import BrownButton from '../../buttons/BrownButton';
+import { EventContent } from './EventContent';
+
+const EventList = ({ events }) => {
+  return (
+    <div className='event_container'>
+      {events ? (
+        events.map((event) => {
+          <EventContent
+            eventImage={false}
+            title={event.eventTitle}
+            content={event.content}
+          />;
+        })
+      ) : (
+        <EventContent
+          EventImag={false}
+          EventTitle={'No Events Yet? Explore Events'}
+          EventContent={
+            "You don't have events history, because you didn’t apply for any events yet. Explore our upcoming events to connect with our family and sell your products"
+          }
+        />
+      )}
+      <BrownButton
+        paddingLeftRight='100'
+        content='Explore Events'
+        link='/events/upcoming-events'
+        maxWidth='500px'
+      />
+    </div>
+  );
+};
+
+export default EventList;
