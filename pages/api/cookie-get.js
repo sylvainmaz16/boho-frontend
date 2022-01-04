d180786ed1fd97288cd52571f505ec8267bf1c71
sylvainmaz16 <sylvainmaz16@gmail.com>
2022-01-03 21:15:15 -0800

    Fixed all linting warnings

--- pages/api/cookie-get.js
+++ pages/api/cookie-get.js
@@ -1,11 +1,11 @@
-import { serverCookieGet } from "../../services/cookies";
+import { serverCookieGet } from '../../services/cookies';
 
-export default async (req, res) => {
+export default async function handler(req, res) {
   // Instead of static content this would be the place for a db request.
 
   const cookie = serverCookieGet(req, res);
   console.log(cookie);
 
   res.statusCode = 200;
-  res.end(JSON.stringify({ message: "success" }));
-};
+  res.end(JSON.stringify({ message: 'success' }));
+}
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -44,7 +44,7 @@ const Dashboard = () => {
   return (
     <>
       <Head>
-        <meta charset='utf-8' />
+        <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <title>Dashboard</title>
         <meta name='description' content='a list of events' />
