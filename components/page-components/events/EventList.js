87741adf1761bc512dc9e7437c3b6fb7dc59fee9
sylvainmaz16 <sylvainmaz16@gmail.com>
2022-01-06 17:53:37 -0800

    changed props to accept the correct prop names

--- components/page-components/events/EventList.js
+++ components/page-components/events/EventList.js
@@ -1,18 +1,19 @@
-import EventCard from './EventCard';
+import EventCard from "./EventCard";
 
 export const EventDashBoardList = ({ events }) => {
   return (
-    <div className='event_list'>
+    <div className="event_list">
       {events.map((event, idx) => {
         return (
           <EventCard
             key={idx}
-            eventImage={event.eventImage}
-            eventTitle={event.eventTitle}
-            eventDates={event.eventDates}
-            eventLength={event.eventLength}
-            eventLocation={event.eventLocation}
-            eventDetails={event.eventDetails}
+            eventImage={event.image_url}
+            eventName={event.name}
+            eventStart={event.start}
+            eventEnd={event.end}
+            eventHours={event.hours}
+            eventLocation={event.location}
+            eventUrl={event.details_url}
           />
         );
       })}
