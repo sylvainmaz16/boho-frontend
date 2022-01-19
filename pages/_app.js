8c85016a551734d859ebcd86f1aefab1163374a5
sylvainmaz16 <sylvainmaz16@gmail.com>
2022-01-18 19:30:16 -0800

    fixing icon error

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta charset="utf-8" />
+        <meta charSet="utf-8" />
       </Head>
       {getLayout(<Component {...pageProps} />)}
     </>
